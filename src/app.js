8eced2d998ca0923691140293837d26be7aab096
kerblammo <31718829+kerblammo@users.noreply.github.com>
2019-02-04 18:09:29 -0400

    Permission Based Commands
    Commands are now configurable with a white/black list. Debug and Vardump commands are configured to accept the list as based in src/commands.json

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ var auth = require('./auth.json');
 var fs = require('fs');
 
 var global = {
-    "users" : null,
-    "commands": null
+    "users": null,
+    "commands": null,
+    "messages": {
+        "lackingPermissions": "ERROR: You do not have permission to use this command"
+    }
 };
 
 //start the application
@@ -27,18 +30,18 @@ logger.level = 'debug';
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
- });
+});
 
 //actions to perform when bot is ready
 bot.on('ready', function (evt) {
-    
+
 
     //get all users who have sent messages
     global.users = JSON.parse(fs.readFileSync("users.json", "utf8"));
-    
+
     //get all commands and associated permissions
     global.commands = JSON.parse(fs.readFileSync("commands.json"));
-    
+
     //log ready message
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -46,10 +49,10 @@ bot.on('ready', function (evt) {
 });
 
 //receive a command
-bot.on('message', function (user, userID, channelID, message, evt){
-    
+bot.on('message', function (user, userID, channelID, message, evt) {
+
     //check if user has been collected yet
-    if (!usersContains(userID)){
+    if (!usersContains(userID)) {
         //collect new user
         usersAdd(user, userID);
     }
@@ -59,7 +62,7 @@ bot.on('message', function (user, userID, channelID, message, evt){
         var cmd = args[0];
 
         //commands accepted
-        switch(cmd){
+        switch (cmd) {
             case 'debug':
                 handleDebugCommand(args, user, userID, channelID, message);
                 break;
@@ -82,8 +85,8 @@ bot.on('message', function (user, userID, channelID, message, evt){
  */
 function usersContains(userID) {
     var found = false;
-    for (var i = 0; i < global.users.users.length; i++){
-        if (global.users.users[i].userID === userID){
+    for (var i = 0; i < global.users.users.length; i++) {
+        if (global.users.users[i].userID === userID) {
             found = true;
             break;
         }
@@ -99,15 +102,15 @@ function usersContains(userID) {
  * @param {string} user The username of the user to add
  * @param {string} userID The user's unique identifier
  */
-function usersAdd(user, userID){
+function usersAdd(user, userID) {
     var usr = {
-        "user" : user,
-        "userID" : userID
+        "user": user,
+        "userID": userID
     }
     global.users.users.push(usr);
     //write to disk for persistence
-    fs.writeFile("users.json", JSON.stringify(global.users), function(err){
-        if (err){
+    fs.writeFile("users.json", JSON.stringify(global.users), function (err) {
+        if (err) {
             logger.error(err);
         } else {
             logger.info('New user added to configuration');
@@ -119,8 +122,8 @@ function usersAdd(user, userID){
  * Actions to perform when a user enters the debug command
  * Prints information about the user who sent the command
  * First checks for accepted arguments: full, user, userID, channel, or help
- * If no accepted arguments are found, instead sends a message suggesint the user use the help command
- * Last update: 2019-02-03
+ * If no accepted arguments are found, instead sends a message suggesing the user use the help command
+ * Last update: 2019-02-04
  * @author Peter Adam <padamckb@hotmail.com>
  * @param {string[]} args The arguments given to the command
  * @param {string} user The command-giver's username
@@ -128,42 +131,68 @@ function usersAdd(user, userID){
  * @param {string} channelID The channel the command was received from
  * @param {string} message The full message body sent
  */
-function handleDebugCommand(args, user, userID, channelID, message){
-    var response = "__Printing debug information:__\n";
-    var executed = false;
-    if (args[1] === "full"){
-        args = ['user', 'userID', 'channel'];
-        
+function handleDebugCommand(args, user, userID, channelID, message) {
+
+    if (isCommandPermittedByUser(global.commands.global, userID, global.commands.debug.mode)
+        && isCommandPermittedByUser(global.commands.debug, userID)) {
+        var response = "__Printing debug information:__\n";
+        var executed = false;
+        var permissionDenied = false;
+        if (args[1] === "full") {
+            if (isCommandPermittedByUser(global.commands.debug.args[0], userID)){
+                args = ['user', 'userID', 'channel'];
+            } else {
+                permissionDenied = true;
+            }
+            
+
+        }
+
+        if (args.includes('user')) {
+            if (isCommandPermittedByUser(global.commands.debug.args[1], userID)){
+                response += "**Username:** " + user + "\n";
+                executed = true;
+            } else {
+                permissionDenied = true;
+            }
+            
+        }
+
+        if (args.includes('userID')) {
+            if (isCommandPermittedByUser(global.commands.debug.args[2], userID)){
+                response += "**User ID:** " + userID + "\n";
+                executed = true;
+            } else {
+                permissionDenied = true;
+            }
+            
+        }
+
+        if (args.includes('channel')) {
+            if (isCommandPermittedByUser(global.commands.debug.args[3], userID)){
+                response += "**Channel:** " + channelID + "\n";
+                executed = true;
+            }
+            
+        }
+
+        if (args.includes('help')
+            || args.includes('info')
+            || args.includes('?')) {
+            //no recognized command found
+            response = formatHelpMessage(global.commands.debug);
+        } else if (permissionDenied){
+            response = global.messages.lackingPermissions;
+        } else if (!executed) {
+            response = formatBadCommandMessage('debug');
+        }
+        logger.info(response);
+        bot.sendMessage({
+            to: channelID,
+            message: response
+        });
     }
 
-    if (args.includes('user')){
-        response += "**Username:** " + user + "\n";
-        executed = true;
-    }
-
-    if (args.includes('userID')){
-        response += "**User ID:** " + userID + "\n";
-        executed = true;
-    }
-
-    if (args.includes('channel')){
-        response += "**Channel:** " + channelID + "\n";
-        executed = true;
-    }
-
-    if (args.includes('help') 
-        || args.includes('info')
-        || args.includes('?')){
-        //no recognized command found
-        response = formatHelpMessage(global.commands.debug);
-    } else if (!executed){
-        response = formatBadCommandMessage('debug');
-    }
-    logger.info(response);
-    bot.sendMessage({
-        to: channelID,
-        message: response
-    });
 }
 
 /**
@@ -171,34 +200,46 @@ function handleDebugCommand(args, user, userID, channelID, message){
  * Prints information about a variable the bot is holding in memory.
  * Accepted arguments include: users and help
  * If no accepted arguments are found, instead sends a message suggesting the user use the help command
- * Last Update: 2019-02-03
+ * Last Update: 2019-02-04
  * @author Peter Adam <padamckb@hotmail.com>
  * @param {string[]} args Arguments supplied to the command
  * @param {string} user Name of user who sent command
  * @param {string} userID Identifier of user who sent command
  * @param {string} channelID Channel command was sent from
  */
-function handleVardumpCommand(args, user, userID, channelID){
-    var response;
-    
-    if (args[1] === "users"){
-        response = "__Printing Users Vardump__\n";
-        for (var i = 0; i < global.users.users.length; i++){
-            response += "**Username:** " + global.users.users[i].user + "\n**UserID**: " + global.users.users[i].userID + "\n\n";
-            
+function handleVardumpCommand(args, user, userID, channelID) {
+
+    if (isCommandPermittedByUser(global.commands.global, userID, global.commands.vardump.mode)
+        && isCommandPermittedByUser(global.commands.vardump, userID)) {
+
+        //user is permitted to use command based on command and global white/blacklists
+        var response;
+        if (args[1] === "users") {
+            if (isCommandPermittedByUser(global.commands.vardump.args[0], userID)) {
+                //vardump of users
+                response = "__Printing Users Vardump__\n";
+                for (var i = 0; i < global.users.users.length; i++) {
+                    response += "**Username:** " + global.users.users[i].user + "\n**UserID**: " + global.users.users[i].userID + "\n\n";
+
+                }
+            } else {
+                //no permission for 'users' argument
+                response = global.messages.lackingPermissions;
+            }
+        } else if (args.includes("help")
+            || args.includes("info")
+            || args.includes("?")) {
+            response = formatHelpMessage(global.commands.vardump);
+        } else {
+            response = formatBadCommandMessage('vardump');
         }
-    } else if (args.includes("help")
-    ||args.includes("info")
-    ||args.includes("?")){
-        response = formatHelpMessage(global.commands.vardump);
-    } else {
-        response = formatBadCommandMessage('vardump');
+        logger.info(response);
+        bot.sendMessage({
+            to: channelID,
+            message: response
+        });
     }
-    logger.info(response);
-    bot.sendMessage({
-        to: channelID,
-        message: response
-    });
+
 }
 
 /**
@@ -208,15 +249,15 @@ function handleVardumpCommand(args, user, userID, channelID){
  * @param {object} command The command to be formatted
  * @returns {string} A formatted help message
  */
-function formatHelpMessage(command){
+function formatHelpMessage(command) {
     //name and description
     var output = "**" + command.name.toUpperCase() + "**\n";
     output += command.description + "\n";
 
     //argument descriptions
-    if (command.args.length > 0){
+    if (command.args.length > 0) {
         output += "Arguments include:\n";
-        for (var i = 0; i < command.args.length; i++){
+        for (var i = 0; i < command.args.length; i++) {
             var arg = command.args[i];
             output += "*" + arg.name + "* - " + arg.description + "\n";
         }
@@ -233,6 +274,34 @@ function formatHelpMessage(command){
  * @param {string} name The name of the command
  * @returns {string} Formatted error message 
  */
-function formatBadCommandMessage(name){
+function formatBadCommandMessage(name) {
     return "Error: unrecognized command structure. Try `" + name + " help` for more information.";
+}
+
+/**
+ * Check if a user is permitted to use a command
+ * Last Update: 2019-02-04
+ * @author Peter Adam <padamckb@hotmail.com>
+ * @param {Object} command The command to be evaluated, read from global.commands
+ * @param {Object[]} command.whitelist Users on whitelist of command
+ * @param {Object[]} command.blacklist Users on blacklist of command
+ * @param {string} command.mode The command's default blacklist vs whitelist setting
+ * @param {string} userID The identifier of the user to check for
+ * @param {string} [mode] The list to check on, default to command.mode 
+ * @returns {boolean} True if the user is permitted to use this command
+ */
+function isCommandPermittedByUser(command, userID, mode = command.mode) {
+    var permitted = mode === "black" ? true : false;
+    var list = permitted ? command.blacklist : command.whitelist;
+    console.log("Command: " + command.name);
+    console.log("Mode: " + mode);
+
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].userID === userID) {
+            permitted = !permitted;
+            console.log("found in list");
+            break;
+        }
+    }
+    return permitted;
 }
